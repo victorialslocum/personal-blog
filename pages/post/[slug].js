71dc430366fe92543c67c6530c3113c53ee3bb6d
Victoria Slocum <victorialslocum@gmail.com>
2023-01-10 15:32:11 -0500

    prettier blog pages

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import matter from 'gray-matter';
 import md from 'markdown-it';
+import Image from "next/image";
 
 export async function getStaticPaths() {
   const files = fs.readdirSync('posts');
@@ -28,8 +29,18 @@ export async function getStaticProps({ params: { slug } }) {
 
 export default function PostPage({ frontmatter, content }) {
   return (
-    <div className='prose px-5 mx-auto'>
-      <h1>{frontmatter.title}</h1>
+    <div className='prose px-5 mx-auto pb-10'>
+      <h1 className='underline decoration-main'>{frontmatter.title}</h1>
+      <div className='flex mb-10'><div className="text-xl px-3 mr-5 bg-gray-200 rounded-md">
+                      {frontmatter.date}
+                    </div><div className="flex-none flex">
+                  {frontmatter.tags.map((tag) => (
+                    <div className="flex-none px-2 mr-2 bg-main-light rounded-md">
+                      {tag}
+                    </div>
+                  ))}
+                </div></div>
+      
       <div dangerouslySetInnerHTML={{ __html: md().render(content) }} />
     </div>
   );
