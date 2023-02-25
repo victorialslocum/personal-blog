c21db036979077f156e77ed58903f6c465e54d64
Victoria Slocum <victorialslocum@gmail.com>
2023-02-25 15:19:20 +0100

    add favicon (?)

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
-import Layout from '../components/layout';
-import '../styles/globals.css';
-import { Analytics } from '@vercel/analytics/react';
+import Layout from "../components/layout";
+import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <link rel="icon" type="image/png" href="/public/favicon.png" />
+      </Head>
       <Component {...pageProps} />
       <Analytics />
     </Layout>
